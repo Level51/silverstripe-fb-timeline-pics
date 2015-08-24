4185fb047ca53d3d1cf5f86961c47f9d2d83aaf0
Julian Scheuchenzuber <js@lvl51.de>
2015-08-24 11:15:50 +0200

    Added getPosts method

--- code/FacebookAPI.php
+++ code/FacebookAPI.php
@@ -66,9 +66,79 @@ class FacebookAPI {
     }
 
     /**
-     * Get all images from the timeline picture album as an array with the fields "id", "name" and "source".
+     * Get all feed post entries from the page as an array with the fields "Message" and "Date".
      * @param int $limit
-     * @return array
+     * @return ArrayList
+     */
+    public function getPosts($limit = 100) {
+        try {
+            // Make the actual API request
+            $response = $this->api->get(SiteConfig::current_site_config()->PageURL . '/feed');
+
+            // Get data of feed
+            $feed = json_decode($response->getBody())->data;
+
+            // Populate ArrayList
+            $fA = ArrayList::create();
+            $entry = array();
+            foreach($feed as $post) {
+                if(!isset($post->message))
+                    continue;
+
+                $entry['Message'] = $this->makeLinks($post->message);
+                $entry['Date'] = date('d.m.Y H:i', strtotime($post->created_time));
+                $fA->add($entry);
+
+                if($fA->count() == $limit) break;
+            }
+
+            return $fA;
+        } catch(FacebookSDKException $e) {
+            user_error($e->getMessage());
+        }
+    }
+
+    /**
+     * Looks for links in a string and "activates" them.
+     * Taken from: http://krasimirtsonev.com/blog/article/php--find-links-in-a-string-and-replace-them-with-actual-html-link-tags
+     * ...thx, man! ;-)
+     * @param $str
+     * @return mixed
+     */
+    private function makeLinks($str) {
+        $reg_exUrl = "/(((http|https|ftp|ftps)\:\/\/)|(www\.))[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\:[0-9]+)?(\/\S*)?/";
+        $urls = array();
+        $urlsToReplace = array();
+        if(preg_match_all($reg_exUrl, $str, $urls)) {
+            $numOfMatches = count($urls[0]);
+            $numOfUrlsToReplace = 0;
+            for($i=0; $i<$numOfMatches; $i++) {
+                $alreadyAdded = false;
+                $numOfUrlsToReplace = count($urlsToReplace);
+                for($j=0; $j<$numOfUrlsToReplace; $j++) {
+                    if($urlsToReplace[$j] == $urls[0][$i]) {
+                        $alreadyAdded = true;
+                    }
+                }
+                if(!$alreadyAdded) {
+                    array_push($urlsToReplace, $urls[0][$i]);
+                }
+            }
+            $numOfUrlsToReplace = count($urlsToReplace);
+            for($i=0; $i<$numOfUrlsToReplace; $i++) {
+                $str = str_replace($urlsToReplace[$i], "<a href=\"".$urlsToReplace[$i]."\" target=\"_blank\">".$urlsToReplace[$i]."</a> ", $str);
+            }
+            return $str;
+        } else {
+            return $str;
+        }
+    }
+
+
+    /**
+     * Get all images from the timeline picture album as an array with the fields "ID", "Name" and "Source".
+     * @param int $limit
+     * @return ArrayList
      */
     public function getFeedPhotos($limit = 100) {
         try {
@@ -78,13 +148,17 @@ class FacebookAPI {
             // Get data of album
             $album = json_decode($response->getBody())->data;
 
-            // Fix decoding of special chars (e.g. German Umlaute)
+            // Populate ArrayList
+            $pA = ArrayList::create();
+            $photo = array();
             foreach($album as $picture) {
-                if(isset($picture->name))
-                    $picture->name = utf8_decode($picture->name);
+                $photo['ID'] = $picture->id;
+                $photo['Source'] = $picture->source;
+                if(isset($picture->name)) $photo['Name'] = $picture->name;
+                $pA->add($photo);
             }
 
-            return $album;
+            return $pA;
         } catch(FacebookSDKException $e) {
             user_error($e->getMessage());
         }
